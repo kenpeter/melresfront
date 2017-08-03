277f7cd32b25513c8bc4a41529b1a05ca34915f1
vpsdime <vpsdime@vpsdime.com>
2017-08-03 12:22:50 +0000

    [up] up

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-  backendRootUrl: 'http://melresback.local',
-  backendSingleResDataUrl: 'http://melresback.local/api/restaurant',
-  backendAuthUrl: 'http://melresback.local/api/auth',
+  backendRootUrl: 'https://melresback.shopshop.space',
+  backendSingleResDataUrl: 'https://melresback.shopshop.space/api/restaurant',
+  backendAuthUrl: 'https://melresback.shopshop.space/api/auth',
   backendUsername: 'kenpeter'
 };
 
