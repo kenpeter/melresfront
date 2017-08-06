810ed5b244dae083ea1d52a8c16af90bca4b7b74
kenpeter <figo2476@hotmail.com>
2017-08-06 11:16:46 +1000

    [up] up

--- src/config.js
+++ src/config.js
@@ -1,17 +1,18 @@
-/*
+
 const config = {
   backendRootUrl: 'https://melresback.shopshop.space',
   backendSingleResDataUrl: 'https://melresback.shopshop.space/api/restaurant',
   backendAuthUrl: 'https://melresback.shopshop.space/api/auth',
   backendUsername: 'kenpeter'
 };
-*/
 
+/*
 const config = {
   backendRootUrl: 'http://melresback.local',
   backendSingleResDataUrl: 'http://melresback.local/api/restaurant',
   backendAuthUrl: 'http://melresback.local/api/auth',
   backendUsername: 'kenpeter'
 };
+*/
 
 module.exports = config;
